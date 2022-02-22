295bb9a3a0fe4ef911fa3e686e4497de56b6219e
Tuan <tuanevk55@gmail.com>
2022-02-22 16:20:06 +0700

    LAB04_1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state={
-      dish: DISHES
-    };
-  }
   render() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-      </Navbar>
-      <Menu dishes={this.state.dish} />
+      <Main />
     </div>
   );
 }
