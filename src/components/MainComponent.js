c73f00000048e3e24d3854d82ee5ece5af91d1e6
Tuan <tuanevk55@gmail.com>
2022-03-07 21:32:06 +0700

    Single page application part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,6 +31,14 @@ class Main extends Component {
         />
       );
     }
+
+    const DishWithId=({match}) => {
+      return(
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id=== parseInt(match.params.dishId,10))[0]}
+                comments={this.state.comments.filter((comment) => comment.dishId=== parseInt(match.params.dishId,10))} />
+
+      );
+    };
   return (
       <div>
       <Header />
@@ -38,6 +46,7 @@ class Main extends Component {
         <Route path="/Home" component={Homepage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
         <Route exact path="/contactus" component={Contact} />
+        <Route path="/menu/:dishId" component={DishWithId} />
         <Redirect to="/Home" />
       </Switch>
       <Footer />
