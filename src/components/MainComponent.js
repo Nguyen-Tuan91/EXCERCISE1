038a2553b15b9d34fd0a9d18e62b4b0cddb8fb77
Tuan <tuanevk55@gmail.com>
2022-03-08 21:02:56 +0700

    update exercise2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -47,6 +48,7 @@ class Main extends Component {
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
         <Route exact path="/contactus" component={Contact} />
         <Route path="/menu/:dishId" component={DishWithId} />
+        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
         <Redirect to="/Home" />
       </Switch>
       <Footer />
