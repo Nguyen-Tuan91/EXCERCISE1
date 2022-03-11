00c3cf0c427273f404e3fe10622d5ebec2823244
Tuan <tuanevk55@gmail.com>
2022-03-11 21:41:08 +0700

    lab06_1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
-    return (
+  return (
+    <BrowserRouter>
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Main />
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
+}
 }
 
 export default App;
