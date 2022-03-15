b92178d0ebfd67796665d9e22ce05cbe5e7847b6
Tuan <tuanevk55@gmail.com>
2022-03-15 16:54:27 +0700

    asm3

--- src/Components/StaffList.js
+++ src/Components/StaffList.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardTitle, CardBody } from 'reactstrap';
+import { Card, CardImg, CardTitle, CardBody, Form, FormGroup, Label, Input, 
+        Col, FormFeedback, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+
+    //Presentational Component dùng để render ra từng nhân viên
     function RenderStaff({staff}) {
         return(
             <Card>
@@ -15,29 +18,70 @@ import { Link } from 'react-router-dom';
             
         );
     }
-        const StaffList=(props) => {
-            const staffList=props.staffs.map((staff) => {
-                console.log(props.staff);
-                return(
-                    <div className="col-lg-2 col-md-4 col-6" key={staff.id}>
-                    <RenderStaff staff={staff} />
+        
+        class StaffList extends Component {
+                constructor(props) {
+                    super(props);
+                    this.state={
+                                nameSearch:''
+                    };
+                
+                    this.handleSearch=this.handleSearch.bind(this);
+                }
+
+                handleSearch(event) {
+                    const nameF=event.target.nameF.value;
+                    event.preventDefault();
+                    this.setState({nameSearch:nameF});
+                }
+
+            render() {
+                const staffList=this.props.staffs
+                .filter((val) => {
+                    if(this.state.nameSearch ==="")
+                    return val;
+                    else if(val.name.toLowerCase().includes(this.state.nameSearch.toLowerCase()))
+                    return val;
+                    return 0;
+                })
+                .map((val) => {
+                    return(
+                        <div className="col-lg-2 col-md-4 col-6" key={val.id}>
+                        <RenderStaff staff={val} />
+                        </div>
+                    );
+                });
+
+                //Render giao diện stafflist
+                 return(
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <h3>Nhân viên</h3>
+                            <hr />
+                        </div>
                     </div>
-                );
-            });
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h3>Nhân viên</h3>
-                        <hr />
+                    <div className="col-12 col-md-6">
+                        <form onSubmit={this.handleSearch} className="form-group row">
+                            <div className="col-8 col-md-8">
+                                <input type="text" name="nameF"
+                                        placeholder="Tìm kiếm tên nhân viên"
+                                />
+                            </div>
+                            <div className="col-4 col-md-4">
+                                <button className="btn btn-success" type="submit" value="submit">Tìm kiếm</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="row">
+                        {staffList}
                     </div>
                 </div>
-                <div className="row">
-                    {staffList}
-                </div>
-            </div>
-        );
-        }
+            );
+            } 
+    }
+        
+        
     
         
 export default StaffList;
