3a1edd0b316656b5af2e8fe56d7215d0d6b159f6
Tuan <tuanevk55@gmail.com>
2022-03-16 00:12:32 +0700

    ASM3

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -23,11 +23,20 @@ class Main extends Component {
                 <StaffDetail staff={this.state.staff.filter((staff) => staff.id===parseInt(match.params.staffId,10))[0]} />
             );
         };
+        const addStaff=(staff) => {
+            const id=Math.floor(Math.random() * 10000 + 1);
+            const newStaff={id,...staff};
+            this.setState({
+                staff:{...this.state.staff, newStaff}
+            });
+            console.log(newStaff);
+            console.log(this.state.staff);
+        }
     return(
         <div>
             <Header />
                 <Switch>
-                    <Route exact path="/staff" component={() => <StaffList staffs={this.state.staff} />} />
+                    <Route exact path="/staff" component={() => <StaffList onAdd={addStaff} staffs={this.state.staff} />} />
                     <Route path="/staff/:staffId" component={StaffWithId} />
                     <Route path="/department" component={() => <Department departments={this.state.department} />} />
                     <Route path="/salary" component={() => <Salary staffs={this.state.staff} />} />
--- src/Components/StaffList.js
+++ src/Components/StaffList.js
@@ -23,12 +23,94 @@ import { Link } from 'react-router-dom';
                 constructor(props) {
                     super(props);
                     this.state={
-                                nameSearch:''
-                    };
-                
+                                nameSearch: '',
+                                id:'',
+                                name:'',
+                                doB:'',
+                                salaryScale:1,
+                                startDate:'',
+                                department:'Sale',
+                                annualLeave:0,
+                                overTime:0,
+                                salary:'',
+                                isModalOpen:false,
+                                touched: {
+                                    name:false,
+                                    doB:false,
+                                    startDate:false
+                                }
+                            };
+                    
                     this.handleSearch=this.handleSearch.bind(this);
+                    this.toggleModal=this.toggleModal.bind(this);
+                    this.handleSubmit=this.handleSubmit.bind(this);
+                    this.handleInputChange=this.handleInputChange.bind(this);
+                    this.handleBlur=this.handleBlur.bind(this);
                 }
 
+                handleInputChange(event) {
+                    const target=event.targe;
+                    const value=target.value;
+                    const name=target.name;
+                    this.setState({
+                        [name]:value
+                    });
+                }
+
+                handleSubmit(event) {
+                    console.log('Current State is:' + JSON.stringify(this.state));
+                    alert('Current State is:' + JSON.stringify(this.state));
+                    event.preventDefault();
+                    const newStaff={
+                        name:this.state.name,
+                        doB:this.state.doB,
+                        salaryScale:this.state.salaryScale,
+                        startDate:this.state.startDate,
+                        department:this.state.department,
+                        annualLeave:this.state.annualLeave,
+                        overTime:this.state.overTime,
+                        image:'/assets/images/pizzaro.png'
+                    }
+                    if(!this.state.name||!this.state.doB||!this.state.startDate)
+                        this.setState({
+                            touched:{name:true, doB:true, startDate:true}
+                        });
+                    else 
+                    this.props.onAdd(newStaff);
+                }
+
+                handleBlur=(field) =>(evt) => {
+                    this.setState({
+                        touched:{...this.state.touched,[field]:true}
+                    });
+                }
+                
+                validate(name, doB, startDate) {
+                   const errors={
+                       name:'',
+                       doB:'',
+                       startDate:''
+                   };
+
+                if(this.state.touched.name&&name.length<3)
+                   errors.name="Must be greater than >=3 characters";
+                else if(this.state.touched && name.length>30)
+                   errors.name="Must be 30 characters or less";
+                
+                if(this.state.touched.doB && doB.length<1)
+                   errors.doB="Please write on";
+                if(this.state.touched.startDate && startDate.length<1)
+                   errors.startDate="Please write on";
+                return errors;
+                }
+
+                /*Hàm bật tắt Modal*/
+                toggleModal() {
+                    this.setState({
+                        isModalOpen: !this.state.isModalOpen
+                    });
+                }
+                /*Hàm tìm kiếm tên nhân viên và render ra kết quả */
                 handleSearch(event) {
                     const nameF=event.target.nameF.value;
                     event.preventDefault();
@@ -36,6 +118,8 @@ import { Link } from 'react-router-dom';
                 }
 
             render() {
+                const errors=this.validate(this.state.name, this.state.doB, this.state.startDate);
+
                 const staffList=this.props.staffs
                 .filter((val) => {
                     if(this.state.nameSearch ==="")
@@ -60,6 +144,11 @@ import { Link } from 'react-router-dom';
                             <h3>Nhân viên</h3>
                             <hr />
                         </div>
+                        <div className="col-2 col-auto">
+                            <Button outline onClick={this.toggleModal}>
+                                <span className="fa fa-plus fa-lg"></span>
+                            </Button>
+                        </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <form onSubmit={this.handleSearch} className="form-group row">
@@ -73,6 +162,89 @@ import { Link } from 'react-router-dom';
                             </div>
                         </form>
                     </div>
+                    <div className="col-12">
+                        <hr />
+                    </div>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+                        <ModalBody>
+                            <Form onSubmit={this.handleSubmit}>
+                                <FormGroup row>
+                                    <Label htmlFor="name" md={2}>Tên</Label>
+                                    <Col md={10}>
+                                        <Input type="text" id="name" name="name"
+                                            value={this.state.name}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleBlur("name")} />
+                                        <FormFeedback>{errors.name}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="doB" md={2}>Ngày Sinh</Label>
+                                    <Col md={10}>
+                                        <Input type="date" id="doB" name="doB"
+                                            value={this.state.doB}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleBlur("doB")} />
+                                        <FormFeedback>{errors.doB}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="startDate" md={2}>Ngày vào công ty</Label>
+                                    <Col md={10}>
+                                        <Input type="date" id="startDate" name="startDate"
+                                            value={this.state.startDate}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleBlur("startDate")} />
+                                        <FormFeedback>{errors.startDate}</FormFeedback>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="salaryScale" md={2}>Hệ số lương</Label>
+                                    <Col md={10}>
+                                        <Input type="text" id="salaryScale" name="salaryScale"
+                                            value={this.state.salaryScale}
+                                            onChange={this.handleInputChange} />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="department" md={2}>Phòng ban</Label>
+                                    <Col md={10}>
+                                        <Input type="select" id="salaryScale" name="salaryScale"
+                                            value={this.state.salaryScale}
+                                            onChange={this.handleInputChange}>
+                                            <option>Sale</option>
+                                            <option>HR</option>
+                                            <option>Marketing</option>
+                                            <option>IT</option>
+                                            <option>Finance</option>
+                                        </Input>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="annualLeave" md={2}>Số ngày nghỉ còn lại</Label>
+                                    <Col md={10}>
+                                        <Input type="text" id="annualLeave" name="annualLeave"
+                                            value={this.state.annualLeave}
+                                            onChange={this.handleInputChange} />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label htmlFor="overTime" md={2}>Số ngày làm thêm</Label>
+                                    <Col md={10}>
+                                        <Input type="text" id="overTime" name="overTime"
+                                            value={this.state.overTime}
+                                            onChange={this.handleInputChange} />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Col md={{size:10, offset:2}}>
+                                        <Button type="submit" color="primary">Thêm</Button>
+                                    </Col>
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+                    </Modal>
                     <div className="row">
                         {staffList}
                     </div>
