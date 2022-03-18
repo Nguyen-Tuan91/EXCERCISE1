c02e72e8e1ba5e881e99b3e565cd9d523ffea2ac
Tuan <tuanevk55@gmail.com>
2022-03-18 16:30:23 +0700

    redux thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment= (dishId, comment, rating, author) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -9,3 +10,27 @@ export const addComment= (dishId, comment, rating, author) => ({
         author: author
     }
 });
+
+export const fetchDishes= () => (dispatch) => {
+
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    },2000);
+}
+
+export const dishesLoading= () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed= (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes= (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
+});
+
