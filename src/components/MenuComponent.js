6bd34ddbd98b62dcd164c7199e30d3e84fb5a687
Tuan <tuanevk55@gmail.com>
2022-03-18 17:24:50 +0700

    React redux form revisited

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } 
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 
-function RenderMenuItem({dish, onClick}) {
+function RenderMenuItem({dish}) {
     return(
         <Card>
         <Link to={`/menu/${dish.id}`}>
